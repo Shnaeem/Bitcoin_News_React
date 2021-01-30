e2bf4ab4159fe01af832bd19ed035ed5c7b22518
Shnaeem <62766356+Shnaeem@users.noreply.github.com>
2021-01-29 22:18:22 -0500

    v1

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import './App.css';
+import NoMatch from './components/NoMatch';
+import Home from './components/Home';
+import BusinessNews from './components/BusinessNews';
+import BitcoinNews from './components/BitcoinNews'
+import { Route, NavLink, Switch } from "react-router-dom";
+import BITCOIN from "./data/bitcoinSeed";
+import BUSINESS from "./data/businessSeed";
+import Data from "./services/data";
+
+function App() {
+  // console.log(BITCOIN);
+  // console.log(BUSINESS);
+  console.log(Data());
+  return (
+    <div className="App">
+      <nav>
+            <NavLink
+              exact
+              to = {"/"}
+            >
+              Home
+            </NavLink >
+            <NavLink to={"/BitcoinNews"}>
+              BitcoinNews
+            </NavLink>
+            <NavLink to={"/BusinessNews"}>
+              BusinessNews
+            </NavLink>
+      </nav>  
+      <main>
+        <Switch>
+          <Route exact path={"/"}>
+            <Home />
+          </Route>
+          <Route path={"/BitcoinNews"}>
+            <BitcoinNews 
+              newslist = {BITCOIN}
+            />
+          </Route>
+          <Route path={ "/BusinessNews"}>
+            <BusinessNews
+              businesslist = {BUSINESS}
+            />
+          </Route>
+          {/* can pass props */}
+          {/* <Route path="posts/:post_id">
+            <Post />
+          </Route> */}
+          {/* cant pass props. everthing is thats not include this is what * */}
+          <Route path={"*"} component={NoMatch} />
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
+
